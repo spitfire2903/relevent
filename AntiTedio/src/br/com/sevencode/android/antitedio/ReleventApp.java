236702943ff1deb60c84c55c49804a7cc8218e1f
Ricardo N. M <nm.ricardo@gmail.com>
2014-10-19 17:05:38 -0200

    up

--- AntiTedio/src/br/com/sevencode/android/antitedio/ReleventApp.java
+++ AntiTedio/src/br/com/sevencode/android/antitedio/ReleventApp.java
@@ -1,8 +1,11 @@
 package br.com.sevencode.android.antitedio;
 
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseObject;
 
 import android.app.Application;
+import br.com.sevencode.android.antitedio.model.Event;
 
 public class ReleventApp extends Application {
 
@@ -15,6 +18,10 @@ public class ReleventApp extends Application {
 		// TODO Auto-generated method stub
 		super.onCreate();
 		
+		ParseObject.registerSubclass(Event.class);
+		
 		Parse.initialize(this, "cdBdah6LvqTkDZ705lxB3bf6teXoEv5NS5UEC2mz", "TilCqTseTA2Tr8MvkQ7zEtCyen86gR87JgCbxkvN");
+		
+		ParseFacebookUtils.initialize(getString(R.string.facebook_app_id));
 	}
 }
